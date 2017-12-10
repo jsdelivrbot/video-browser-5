83d6c06ee34321a6c83d5aa963f609b41de593ab
Jake Rider <jakerider96@icloud.com>
2017-12-09 23:16:45 -0500

    Structed components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCxn5UQFVUkaEbP9fkbWVw43EN0Te4RSw4';
 
 // Create a new component.
-const App = () => <div>Hi!</div>;
+function App() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
 // Add the component to the DOM.
 ReactDOM.render(<App />, document.querySelector('.container'));
