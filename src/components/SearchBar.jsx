c689bb14f91a7e47575ab23833a32bfd08c017ef
Jake Rider <jakerider96@icloud.com>
2017-12-10 00:47:09 -0500

    Created SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
